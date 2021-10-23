0fb9ab7a43758ac8fb693cccfebc5c88c6064308
karim gomez <karim.gomez29@yahoo.com>
2021-10-23 02:31:10 -0400

    finished db file

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -19,3 +19,58 @@ request.onupgradeneeded = function (e) {
 };
 
 
+request.onerror = function (error) {
+    console.log("Wow" `${error.target.errorcode}`)
+};
+
+let checkdatabase = () => {
+    console.log("DB invoked")
+    let transaction = db.transaction(["BudgetStore"], readwrite);
+    const store = transaction.objectStore("BudgetStore");
+    let getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then((response) => response.json())
+                .then((res) => {
+
+                    if (res.length !== 0) {
+
+                        transaction = db.transaction(['BudgetStore'], 'readwrite');
+                        const currentStore = transaction.objectStore('BudgetStore');
+
+                        currentStore.clear();
+                        console.log('Clearing store 🧹');
+                    }
+                });
+        }
+    };
+
+}
+
+request.onsuccess = function (e) {
+    console.log("sucess")
+    db = e.target.result;
+
+    if (navigator.onLine) {
+        console.log("BackEnd Online");
+        checkdatabase()
+    }
+};
+
+const saveRecord = (record) => {
+    console.log("save record invoked");
+    const transaction = db.transaction(["BudgetStore"], readwrite);
+    const store = transaction.objectStore("BudgetStore");
+    store.add(record);
+};
+window.addEventListener('online', checkdatabase);
